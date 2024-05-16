880b6ca94aeca61aa72aabebe555e612c85ac2ed
Ykb21 <167885752+Ykb21@users.noreply.github.com>
2024-05-16 21:05:56 +0700

    Update destructuring.js

--- exercise/destructuring.js
+++ exercise/destructuring.js
@@ -4,3 +4,10 @@ let b;
 [a, b] = [10, 20]; // array destructuring
 
 console.log(a)
+
+const person = { name: "Budi", age: 20 };
+
+const { name, age } = person // object destructuring
+
+console.log(name)
+console.log(age)
